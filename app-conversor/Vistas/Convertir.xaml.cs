da42ec1a4cc8757e8b6136cfe7089b51627e83f8
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2023-06-29 17:01:17 -0400

    Agregar archivos de proyecto.

---
+++ app-conversor/Vistas/Convertir.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace app_conversor.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        double cm;
+        double m;
+
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular()
+        {
+            cm = Convert.ToDouble(textcm.Text);
+            m = cm / 100;
+            lblresultado.Text = m.ToString() + " metros";
+        }
+
+        private void Validar()
+        {
+            if(!string.IsNullOrEmpty(textcm.Text))
+            {
+                Calcular();
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad", "Ok");
+            }
+        }
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+            Validar();
+        }
+    }
+}
---
+++ app-conversor/Vistas/Menuprincipal.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app_conversor.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menuprincipal : ContentPage
+    {
+        public Menuprincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Convertir() { });
+        }
+
+
+    }
+}
